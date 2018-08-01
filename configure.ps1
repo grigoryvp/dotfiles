cb408a454389a975cb90c29f8b940aae18682409
Grigory <grigory.v.p@gmail.com>
2018-08-01 09:38:53 +0300

    Download autohotkey script

--- configure.ps1
+++ configure.ps1
@@ -5,6 +5,6 @@ $url = 'https://autohotkey.com/download/ahk-install.exe'
 $file = "$env:temp\ahk-install.exe"
 $client = (new-object net.webclient)
 $client.headers.add('User-Agent', 'curl/7.55.1')
-# $client.downloadfile($url, $file)
+$client.downloadfile($url, $file)
 & $file /S
 start keyboard.ahk

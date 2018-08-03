a117685471c2a10184991b88a6d9c5f8b2dbed13
Grigory <grigory.v.p@gmail.com>
2018-08-03 13:39:25 +0300

    Powershell syntax fix

--- configure.ps1
+++ configure.ps1
@@ -5,11 +5,13 @@ cd $env:USERPROFILE
 & ssh-keygen -N '""' -f .ssh/id_rsa
 
 # Install AutoHotkey
-$file = .\ahk-install.exe
+$file = ".\ahk-install.exe"
+echo "Downloading AutoHotkey"
 curl -OutFile $file -Uri https://autohotkey.com/download/ahk-install.exe
 & $file /S
 rm $file
 
 # Modify keyboard
+echo "Downloading keyboard script"
 curl -OutFile keyboard.ahk -Uri https://raw.githubusercontent.com/grigoryvp/my-win-box-cfg/master/keyboard.ahk
 start .\keyboard.ahk

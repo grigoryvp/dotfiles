2ecc394f2d1c8ff28aa5445bf2018bdc17ec9b10
Grigory <grigory.v.p@gmail.com>
2018-08-07 09:11:34 +0300

    Simplified installation script

--- configure.ps1
+++ configure.ps1
@@ -1,43 +1,24 @@
-function isInstalled($appname) {
-  $keyList = @(
-    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall",
-    "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall")
-  foreach($key in $keyList) {
-    $appList = get-childItem $key | where-object {
-      $_.getValue("DisplayName") -like "*$appname*"
-    }
-    if ($appList.length -gt 0) {
-      return $true
-    }
-  }
-  return $false
-}
-
 cd $env:USERPROFILE
 
-if (!(get-command scoop -errorAction SilentlyContinue)) {
-  iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
-  scoop install git
-  scoop update
+if (!(Get-Command scoop -ErrorAction SilentlyContinue)) {
+  Invoke-Expression (New-Object Net.WebClient).DownloadString('https://get.scoop.sh')
+  scoop bucket add extras
 }
 
-if (!(test-path .ssh\id_rsa)) {
-  if (!(test-path .ssh)) {
-    mkdir .ssh
+scoop install autohotkey
+
+if (!(Test-Path .ssh\id_rsa)) {
+  if (!(Test-Path .ssh)) {
+    New-Item -Path .ssh -ItemType Directory
   }
-  start-process ssh-keygen -argumentList '-N "" -f .ssh/id_rsa' -wait
-}
-
-# Install AutoHotkey
-if (!isInstalled("autohotkey")) {
-  $file = ".\ahk-install.exe"
-  write-output "Downloading AutoHotkey"
-  invoke-webRequest -OutFile $file -Uri https://autohotkey.com/download/ahk-install.exe
-  start-process $file -argumentList '/S' -verb RunAs -wait
-  rm $file
+  Start-Process ssh-keygen -ArgumentList '-N "" -f .ssh/id_rsa' -Wait
 }
 
 # Modify keyboard
-write-output "Downloading keyboard script"
-Invoke-WebRequest -OutFile keyboard.ahk -Uri https://raw.githubusercontent.com/grigoryvp/my-win-box-cfg/master/keyboard.ahk
-Start-Process .\keyboard.ahk
+Write-Output "Downloading keyboard script"
+$uri = 'https://raw.githubusercontent.com/grigoryvp/my-win-box-cfg/master/keyboard.ahk'
+Invoke-WebRequest -OutFile keyboard.ahk -Uri $uri
+
+Write-Host -NoNewLine "Press any key to elevate the keyboard script..."
+[System.Console]::ReadKey()
+Start-Process autohotkey.exe -ArgumentList '.\keyboard.ahk' -Verb RunAs -WindowStyle Hidden

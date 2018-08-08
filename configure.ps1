04b7497c6cc3f587a532532a31c36cbd500deaf1
Grigory <grigory.v.p@gmail.com>
2018-08-08 08:30:09 +0300

    Configure power options

--- configure.ps1
+++ configure.ps1
@@ -1,5 +1,15 @@
 cd $env:USERPROFILE
 
+Write-Host "Configuring power options..."
+powercfg -change -monitor-timeout-ac 120
+powercfg -change -monitor-timeout-dc 120
+powercfg -change -disk-timeout-ac 0
+powercfg -change -disk-timeout-dc 0
+powercfg -change -standby-timeout-ac 0
+powercfg -change -standby-timeout-dc 0
+powercfg -change -hibernate-timeout-ac 0
+powercfg -change -hibernate-timeout-dc 0
+
 if (!(Get-Command scoop -ErrorAction SilentlyContinue)) {
   Invoke-Expression (New-Object Net.WebClient).DownloadString('https://get.scoop.sh')
 }

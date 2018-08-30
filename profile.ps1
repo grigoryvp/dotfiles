5d5d181f1e377fe80cf736a915357417f1b758f0
Grigory Petrov <grigory.v.p@gmail.com>
2018-08-30 10:48:41 +0300

    Aliases for fast command access

--- profile.ps1
+++ profile.ps1
@@ -1,1 +1,5 @@
-Import-Module posh-git
+Import-Module posh-git;
+
+function cdd() { Set-Location ~/Documents; }
+function cdc() { Set-Location ~/Documents/PowerShell; }
+function cdh() { Set-Location ~; }

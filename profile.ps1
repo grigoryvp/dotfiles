c0cfaa8b617fe5d1e8e20795ba9c2d893da50d27
Grigory Petrov <grigory.v.p@gmail.com>
2018-08-30 11:42:13 +0300

    More useful shortcuts

--- profile.ps1
+++ profile.ps1
@@ -3,6 +3,7 @@ Import-Module posh-git;
 function cdd() { Set-Location ~/Documents; }
 function cdc() { Set-Location ~/Documents/PowerShell; }
 function cdh() { Set-Location ~; }
+function cdx() { Set-Location ~/Documents/PowerShell/xi; }
 
 function gst() {
   $dirNameList = Get-ChildItem -Name -Directory;

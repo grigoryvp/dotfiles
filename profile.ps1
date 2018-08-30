0ffc1eed58100050027a7c7811fd0208495453b7
Grigory Petrov <grigory.v.p@gmail.com>
2018-08-30 11:28:58 +0300

    git utility command to change status for multiple dirs

--- profile.ps1
+++ profile.ps1
@@ -3,3 +3,29 @@ Import-Module posh-git;
 function cdd() { Set-Location ~/Documents; }
 function cdc() { Set-Location ~/Documents/PowerShell; }
 function cdh() { Set-Location ~; }
+
+function gst() {
+  $dirNameList = Get-ChildItem -Name -Directory;
+  foreach ($dirName in $dirNameList) {
+    Set-Location $dirName;
+    $ret = & git status;
+    $UP_MARKER = "Your branch is up to date with";
+    $NOCOMMIT_MARKER = "nothing to commit, working tree clean";
+    $msg = "";
+    if ($ret[1].Contains($UP_MARKER)) {
+      $msg += "up to date; ";
+    }
+    else {
+      $msg += "out of sync; ";
+    }
+    if ($ret[3].Contains($NOCOMMIT_MARKER)) {
+      $msg += "clean; ";
+    }
+    else {
+      $msg += "changes; ";
+    }
+    $msg += $dirName;
+    Write-Host $msg;
+    Set-Location ..;
+  }
+}

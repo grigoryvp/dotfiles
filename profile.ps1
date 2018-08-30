112e52b6d1b7d0bfc3b3bb2929d3c0e60a87d16a
Grigory Petrov <grigory.v.p@gmail.com>
2018-08-30 11:30:26 +0300

    Better, but still naive parser

--- profile.ps1
+++ profile.ps1
@@ -10,7 +10,7 @@ function gst() {
     Set-Location $dirName;
     $ret = & git status;
     $UP_MARKER = "Your branch is up to date with";
-    $NOCOMMIT_MARKER = "nothing to commit, working tree clean";
+    $CLEAN_MARKER = "nothing to commit, working tree clean";
     $msg = "";
     if ($ret[1].Contains($UP_MARKER)) {
       $msg += "up to date; ";
@@ -18,7 +18,7 @@ function gst() {
     else {
       $msg += "out of sync; ";
     }
-    if ($ret[3].Contains($NOCOMMIT_MARKER)) {
+    if ($ret[3].Contains($CLEAN_MARKER) -or $ret[4].Contains($CLEAN_MARKER)) {
       $msg += "clean; ";
     }
     else {

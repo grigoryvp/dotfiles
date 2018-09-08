b00a963b378655a97f95b65cd4e540ec2a2efa00
Girogry Petrov <grigory.v.p@gmail.com>
2018-09-08 17:37:50 +0300

    VSCode extension debug helper

--- profile.ps1
+++ profile.ps1
@@ -17,7 +17,6 @@ function gst() {
     $ret = & git status;
     $UP_MARKER = "Your branch is up to date with";
     $CLEAN_MARKER = "nothing to commit, working tree clean";
-    $msg = "";
     if ($ret[1].Contains($UP_MARKER)) {
       Write-Host "up to date; " -NoNewline;
     }
@@ -44,4 +43,38 @@ function ahk() {
     -ArgumentList "$($env:USERPROFILE)\Documents\PowerShell\keyboard.ahk" `
     -WindowStyle Hidden `
     -Verb RunAs;
+}
+
+
+function Update-VscodeExt() {
+  $cfgFileName = "package.json";
+  if (-not (Test-Path $cfgFileName)) {
+    Write-Error "$cfgFileName not found";
+    return;
+  }
+  $cfg = Get-Content $cfgFileName | ConvertFrom-Json;
+
+  $name = $cfg.name;
+  if (-not $name) {
+    Write-Error "'name' property not found in the $cfgFileName";
+    return;
+  }
+  $publisher = $cfg.publisher;
+  if (-not $publisher) {
+    Write-Error "'publisher' property not found in the $cfgFileName";
+    return;
+  }
+  $version = $cfg.version;
+  if (-not $version) {
+    Write-Error "'version' property not found in the $cfgFileName";
+    return;
+  }
+
+  $extDir = "$env:USERPROFILE\.vscode\extensions\$publisher.$name-$version";
+  if (-not (Test-Path $extDir)) {
+    Write-Error "'$extDir' directory not found";
+    return;
+  }
+
+  Copy-Item *.js $extDir;
 }

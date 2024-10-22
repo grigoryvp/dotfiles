6c1be1c5835d8c68e7a4d9ded2cf58b0b53f8b99
Grigory Petrov <grigoryvp@gmail.com>
2024-10-22 17:04:21 +0200

    Since same text strings are used to render statusbar it's viable to introduce a simple cache to reduce the cpu load

--- hammerspoon/helpers.lua
+++ hammerspoon/helpers.lua
@@ -1,3 +1,10 @@
-dir = function (o) for k, v in pairs(o) do print(k, "=>", v) end end
-idir = function (o) for i, v in ipairs(o) do print(i, "~>", v) end end
+dir = function (v) for k, v in pairs(v) do print(k, "=>", v) end end
+idir = function (v) for i, v in ipairs(v) do print(i, "~>", v) end end
+function count(v)
+  local sum = 0
+  for _ in pairs(v) do
+    sum = sum + 1
+  end
+  return sum
+end
 cls = hs.console.clearConsole

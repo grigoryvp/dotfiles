98bc0759a0f1ffa6f5a1156f018386ab1ec784e1
Grigory Petrov <gpetrov@walletteam.org>
2026-06-26 09:27:45 +0200

    track cwd and validate commands like 'rm' and 'unzip'

--- .claude/hooks/bash.py
+++ .claude/hooks/bash.py
@@ -7,6 +7,9 @@ import os
 import sys
 import json
 import builtins
+from dataclasses import dataclass
+from pathlib import Path
+from textwrap import dedent
 
 import tree_sitter_bash as tsbash
 from tree_sitter import Language, Parser
@@ -26,14 +29,23 @@ class NotAllowed:
 
 class AskPermission:
 
-    def __init__(self, reason):
+    def __init__(self, reason, state):
         self._reason = reason
+        self._state = state
 
     def __str__(self):
-        return f"Ask permission: {self._reason}"
+        return dedent(f"""
+          Ask permission:
+            cmd: {self._reason}
+            cwd: {self._state.cwd}
+        """.strip("\n"))
 
     def __bool__(self):
         return False
+
+@dataclass
+class State:
+    cwd: str | None = None
 
 
 def json_from_stdin():
@@ -88,9 +100,8 @@ def is_git_command_allowed(args: list[str]):
     except IndexError:
         return NotAllowed(f"Incorrect git args: {" ".join(src_args)}")
 
-def is_command_allowed(sequence: list[str]):
+def is_command_allowed(sequence: list[str], state: State):
     ALLOWED = [
-        "cd",
         "ls",
         "echo",
         "cat",
@@ -103,8 +114,10 @@ def is_command_allowed(sequence: list[str]):
         "find",
         "grep",
         "sort",
+        "javap",  # java disassembler
     ]
     NEED_ARGS = [
+        "cd",
         "xargs",
         "sh",
         "npx",
@@ -120,6 +133,46 @@ def is_command_allowed(sequence: list[str]):
     if cmd in NEED_ARGS and len(args) < 1:
         return NotAllowed(f"{cmd} without args")
 
+    if cmd == "cd":
+        if (args[0].startswith("$")):
+            return AskPermission(f"cd {args[0]}", state)
+        # Save to validate commands like 'rm' in the future
+        path = Path(args[0])
+        if path.is_absolute():
+            state.cwd = str(path)
+        else:
+            state.cwd = str(Path(state.cwd or ".") / path)
+        return True
+    if cmd == "timeout":
+        if len(args) <= 1:
+            return True
+        args.pop(0)  # timeout value
+        return is_command_allowed(args, state)
+    if cmd == "rm":
+        while len(args):
+            if args[0] in ("-r", "-f", "-rf"):
+                args.pop(0)
+                continue
+            if args[0].startswith("-"):
+                return AskPermission(f"rm {args[0]}", state)
+            break
+        for arg in args:
+            path = Path(arg)
+            if not path.is_absolute():
+                path = Path(state.cwd or ".") / path
+            if path.is_relative_to("/tmp"):
+                continue
+            return AskPermission(f"rm {path}", state)
+        return True
+    if cmd == "mkdir":
+        for arg in args:
+            path = Path(arg)
+            if not path.is_absolute():
+                path = Path(state.cwd or ".") / path
+            if path.is_relative_to("/tmp"):
+                continue
+            return AskPermission(f"mkdir {path}", state)
+        return True
     if cmd == "xargs":
         while len(args):
             subarg = args[0]
@@ -127,16 +180,11 @@ def is_command_allowed(sequence: list[str]):
                 args.pop(0)
             else:
                 break
-        return is_command_allowed(args)
+        return is_command_allowed(args, state)
     if cmd == "sh":
         subcmd = args.pop(0)
         if subcmd == "-c":
-            return is_command_allowed(args)
-    if cmd == "timeout":
-        if len(args) <= 1:
-            return True
-        args.pop(0)  # timeout value
-        return is_command_allowed(args)
+            return is_command_allowed(args, State(cwd=state.cwd))
     if cmd == "npx":
         subcmd = args.pop(0)
         if subcmd == "prettier":
@@ -152,21 +200,32 @@ def is_command_allowed(sequence: list[str]):
             return True
     if cmd == "git":
         return is_git_command_allowed(args)
-    return AskPermission(" ".join(sequence))
+    if cmd == "unzip":
+        while len(args):
+            if args[0] in ("-o", "-q"):
+                args.pop(0)
+                continue
+            if args[0].startswith("-"):
+                return AskPermission(f"rm {args[0]}", state)
+            break
+        if Path(state.cwd or ".").is_relative_to("/tmp"):
+            return True
+
+    return AskPermission(" ".join(sequence), state)
 
 
 def node_text(node, source) -> str:
     return source[node.start_byte:node.end_byte].decode("utf-8")
 
 
-def walk_ast(node, source, decisions):
+def walk_ast(node, source, state, decisions):
     if node.type in ("command", "simple_command"):
         # first child is of type "command_name"
         sequence = map(lambda v: node_text(v, source), node.children)
-        decision = is_command_allowed(list(sequence))
+        decision = is_command_allowed(list(sequence), state)
         decisions.append(decision)
     for child in node.children:
-        walk_ast(child, source, decisions)
+        walk_ast(child, source, state, decisions)
 
 
 request = json_from_stdin()
@@ -176,8 +235,9 @@ if not request:
 parser = Parser(Language(tsbash.language()))
 source = request["tool_input"]["command"].encode("utf-8")
 tree = parser.parse(source)
+state = State()
 decisions = []
-walk_ast(tree.root_node, source, decisions)
+walk_ast(tree.root_node, source, state, decisions)
 
 if all(decisions):
     print(json.dumps({
